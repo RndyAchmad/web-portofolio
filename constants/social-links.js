0dc98bf85aa84bedb9082b6bab627b2fbf087c34
Rndyachmad <rendyachmad55@gmail.com>
2026-05-04 11:58:53 +0700

    fix: bug ahref email contact

--- constants/social-links.js
+++ constants/social-links.js
@@ -1,7 +1,7 @@
 export const SOCIAL_LINKS = [
     {
         name: "Email",
-        href: "mailto:rendyachmad55@gmail.com?subject=Collaboration%20Inquiry",
+        href: "https://mail.google.com/mail/?view=cm&fs=1&to=rendyachmad55@gmail.com&su=Permohonan%20Diskusi%20Kerja%20Sama&body=Halo%20Rendy,%0A%0ASaya%20tertarik%20untuk%20berdiskusi%20terkait%20potensi%20kerja%20sama%20dengan%20Anda.%0A%0AJika%20berkenan,%20silakan%20informasikan%20waktu%20yang%20tersedia%20untuk%20diskusi%20lebih%20lanjut.%0A%0ATerima%20kasih.",
         icon: "fas fa-envelope",
         external: true,
     },
